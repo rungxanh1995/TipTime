95fd99db2fd7517b789795d2b2785aa2effb2513
Joe Pham <rungxanh2901@gmail.com>
2021-12-29 17:24:07 -0500

    Optimize imports of test file

--- app/src/androidTest/java/io/github/rungxanh1995/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/io/github/rungxanh1995/tiptime/CalculatorTests.kt
@@ -1,16 +1,13 @@
 package io.github.rungxanh1995.tiptime
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
